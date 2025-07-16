629627946e0f32d0e39d88ff08f2e1c031b0dc57
Manuj Chadha <manujchadha7777@gmail.com>
2025-07-16 09:52:14 +0530

    Update CorsConfig.java

--- src/main/java/com/journal/backend/config/CorsConfig.java
+++ src/main/java/com/journal/backend/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration cors = new CorsConfiguration();
         cors.setAllowCredentials(true);
-        cors.setAllowedOriginPatterns(List.of("http://localhost:5173", "https://reflect-journal-web.vercel.app/")); // ✅ THIS
+        cors.setAllowedOriginPatterns(List.of("http://localhost:5173", "https://reflect-journal-web.vercel.app"));
         cors.setAllowedHeaders(List.of("*"));
         cors.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 
